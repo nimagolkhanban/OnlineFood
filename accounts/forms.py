803d3e86d7f419389307afe89123827c037130ac
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-04-19 12:39:32 +0330

    add validation errors in registration form and html page

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,16 @@ from accounts.models import User
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'phone_number', 'password', 'confirm_password']
+
+    # tip : use clean for cleaning two or three field in the form that depend on each-other
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password != confirm_password:
+            raise forms.ValidationError("passwords don't match")
+        return cleaned_data
