1a2697090e750ac143f4be4a409cda2c0bd5cf58
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-04-26 06:24:29 +0330

    reset password and forgot password is done with email that contain token and uid

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,15 +3,22 @@ from . import views
 
 
 urlpatterns = [
+    # register and activate account
     path('registeruser/', views.RegisterUserView.as_view(), name='registeruser'),
     path('registervendor/', views.RegisterVendorView.as_view(), name='registervendor'),
-
+    path('activate/<uidb64>/<token>/', views.activate_account, name='activate'),
+    # log in and out
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    # dashboard redirectors
     path('myaccount/', views.myaccount, name='myaccount'),
     path('customerdashboard/', views.CustomerDashboardView.as_view(), name='customerdashboard'),
     path('vendordashboard/', views.VendorDashboardView.as_view(), name='vendordashboard'),
-    path('activate/<uidb64>/<token>/', views.activate_account, name='activate'),
+    # forgot password
+    path('forgotpassword/', views.ForgotPasswordView.as_view(), name='forgotpassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.ResetPasswordValidateView.as_view(), name='resetpasswordvalidate'),
+    path('resetpassword/', views.ResetPasswordView.as_view(), name='resetpassword'),
+
 ]
 
 
