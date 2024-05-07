f89061b7cd9373733896e448ebba85cef262fa5e
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-05-07 21:58:15 +0330

    a really interesting change in form that will clean the data and debug our form submitting bug

--- menu/forms.py
+++ menu/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from menu.models import Category
+from menu.models import Category, FoodItem
 
 
 class CategoryForm(forms.ModelForm):
@@ -8,10 +8,39 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ['category_name', 'description']
 
-    def clean_category_name(self):
-        current_category_name = self.cleaned_data['category_name']
-        flag = Category.objects.filter(category_name__iexact=current_category_name).exists()
-        if flag:
-            raise forms.ValidationError('there is a category with this name you pick')
-        return current_category_name
+    # tip: the below code is a veeeery interesting one because it's a generator that check in a dictionary called
+    # changed_data this dictionary contain all the data that changed in the form, and we can use it in edit form
+    # this is very handi tool so remember how to use it wisely
+    def clean(self):
+        if any(field_name in self.changed_data for field_name in ['description']):
+            pass
+        else:
+            current_category_name = self.cleaned_data['category_name']
+            flag = Category.objects.filter(category_name__iexact=current_category_name).exists()
+            if flag:
+                raise forms.ValidationError('there is a category with this name you pick')
+            return current_category_name
+
+
+class FoodForm(forms.ModelForm):
+    class Meta:
+        model = FoodItem
+        fields = ['food_title', 'description', 'price', 'image', 'available']
+
+    # tip: the below code is a veeeery interesting one because it's a generator that check in a dictionary called
+    # changed_data this dictionary contain all the data that changed in the form, and we can use it in edit form
+    # this is very handi tool so remember how to use it wisely
+
+    def clean(self):
+        if any(field_name in self.changed_data for field_name in ['description', 'price', 'image', 'available']):
+            pass
+        else:
+            current_category_name = self.cleaned_data['category_name']
+            flag = Category.objects.filter(category_name__iexact=current_category_name).exists()
+            if flag:
+                raise forms.ValidationError('there is a category with this name you pick')
+            return current_category_name
+
+
+
 
