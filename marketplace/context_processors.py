0e4fc00a725c45e1635b91f0cc4a08f0ad297a5d
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-05-17 14:36:12 +0330

    dynamic trax implemented

--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -1,3 +1,5 @@
+from decimal import Decimal, ROUND_HALF_UP
+
 from .models import Cart
 from menu.models import FoodItem
 
@@ -17,14 +19,30 @@ def get_cart_counter(request):
     return {'cart_count': cart_count}
 
 
+from decimal import Decimal, ROUND_HALF_UP
+
+
+# تابع برای رند کردن Decimal با دو رقم اعشار
+
+
 def get_cart_amounts(request):
-    subtotal = 0
-    tax = 0
-    grand_total = 0
+    subtotal = Decimal('0.0')
+    grand_total = Decimal('0.0')
+    tax_rate = Decimal('0.10')
+
+    def round_decimal(value):
+        return value.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
         for item in cart_items:
             fooditems = FoodItem.objects.get(pk=item.fooditem.pk)
-            subtotal += fooditems.price * item.quantity
+            subtotal += fooditems.price * Decimal(str(item.quantity))
+        tax = subtotal * tax_rate
         grand_total = subtotal + tax
+
+        tax = round_decimal(tax)
+        grand_total = round_decimal(grand_total)
+
     return dict(subtotal=subtotal, tax=tax, grand_total=grand_total)
+
