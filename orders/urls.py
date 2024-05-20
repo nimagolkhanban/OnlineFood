f4b1b84e89a5906f529317ce7b16adb24af19fbb
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-05-21 03:22:17 +0330

    *** add the functionality to create a pdf out of a html page ***

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('order-complete/<int:order_number>/', views.OrderComplete.as_view(), name='order-complete')
+    path('order-complete/<int:order_number>/', views.OrderComplete.as_view(), name='order-complete'),
+    path('pdf_view/<int:order_number>/', views.ViewPDF.as_view(), name="pdf_view"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render, redirect
 from django.views import View
 
 from orders.models import Order, OrderedFood
+from django.shortcuts import render
+from io import BytesIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.views import View
+from xhtml2pdf import pisa
+
+
+
 
 
 class OrderComplete(View):
@@ -16,6 +25,7 @@ class OrderComplete(View):
                 subtotal += (food.price * food.quantity)
             tax = subtotal * 0.1
             grand_total = subtotal + tax
+
         except:
             messages.error(request, 'there is something wrong with your order')
             return redirect('checkout')
@@ -27,4 +37,58 @@ class OrderComplete(View):
             'tax': tax,
             'grand_total': grand_total,
         }
+
         return render(request, 'orders/order_complete.html', context)
+
+
+# tip: in this section we build a pdf out of user order
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+
+
+# Opens up page as PDF
+class ViewPDF(View):
+    def get(self, request, order_number):
+        order_number = order_number
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_food = OrderedFood.objects.filter(order=order)
+        subtotal = 0
+        for food in ordered_food:
+            subtotal += (food.price * food.quantity)
+        tax = subtotal * 0.1
+        grand_total = subtotal + tax
+        data = {
+            'order': order,
+            'ordered_food': ordered_food,
+            'subtotal': subtotal,
+            'tax': tax,
+            'grand_total': grand_total,
+        }
+        pdf = render_to_pdf('orders/pdf_order.html', data)
+        return HttpResponse(pdf, content_type='application/pdf')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
